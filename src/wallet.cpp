b5c6125ec01c27588c7ab15cff7cd5204957beea
Kraitcoin <37198536+KRAITcoin@users.noreply.github.com>
2018-03-18 13:03:34 +0300

    fix mn

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1626,13 +1626,13 @@ void CWallet::AvailableCoins(vector<COutput>& vCoins, bool fOnlyConfirmed, const
                 if (nCoinType == ONLY_DENOMINATED) {
                     found = IsDenominatedAmount(pcoin->vout[i].nValue);
                 } else if (nCoinType == ONLY_NOT10000IFMN) {
-                    found = !(fMasterNode && pcoin->vout[i].nValue == 3000 * COIN);
+                    found = !(fMasterNode && pcoin->vout[i].nValue == 3000 * COIN); //KRAIT
                 } else if (nCoinType == ONLY_NONDENOMINATED_NOT10000IFMN) {
-                    if (IsCollateralAmount(pcoin->vout[i].nValue)) continue; // do not use collateral amounts
+                    if (IsCollateralAmount(pcoin->vout[i].nValue)) continue; // do not use collateral amounts 
                     found = !IsDenominatedAmount(pcoin->vout[i].nValue);
-                    if (found && fMasterNode) found = pcoin->vout[i].nValue != 3000 * COIN; // do not use Hot MN funds
+                    if (found && fMasterNode) found = pcoin->vout[i].nValue != 3000 * COIN; // do not use Hot MN funds  KRAIT
                 } else if (nCoinType == ONLY_10000) {
-                    found = pcoin->vout[i].nValue == 3000 * COIN;
+                    found = pcoin->vout[i].nValue == 3000 * COIN; //KRAIT
                 } else {
                     found = true;
                 }
@@ -2097,7 +2097,7 @@ bool CWallet::SelectCoinsDark(CAmount nValueMin, CAmount nValueMax, std::vector<
         if (out.tx->vout[out.i].nValue < CENT) continue;
         //do not allow collaterals to be selected
         if (IsCollateralAmount(out.tx->vout[out.i].nValue)) continue;
-        if (fMasterNode && out.tx->vout[out.i].nValue == 3000 * COIN) continue; //masternode input
+        if (fMasterNode && out.tx->vout[out.i].nValue == 3000 * COIN) continue; //masternode input KRAIT
 
         if (nValueRet + out.tx->vout[out.i].nValue <= nValueMax) {
             CTxIn vin = CTxIn(out.tx->GetHash(), out.i);
--- src/wallet.h
+++ src/wallet.h
@@ -81,7 +81,7 @@ enum AvailableCoinsType {
     ALL_COINS = 1,
     ONLY_DENOMINATED = 2,
     ONLY_NOT10000IFMN = 3,
-    ONLY_NONDENOMINATED_NOT10000IFMN = 4, // ONLY_NONDENOMINATED and not 10000 KRAIT at the same time
+    ONLY_NONDENOMINATED_NOT10000IFMN = 4, // ONLY_NONDENOMINATED and not 3000 KRAIT at the same time
     ONLY_10000 = 5,                        // find masternode outputs including locked ones (use with caution)
     STAKABLE_COINS = 6                          // UTXO's that are valid for staking
 };
@@ -1052,7 +1052,7 @@ public:
             const CTxIn vin = CTxIn(hashTx, i);
 
             if (pwallet->IsSpent(hashTx, i) || pwallet->IsLockedCoin(hashTx, i)) continue;
-            if (fMasterNode && vout[i].nValue == 10000 * COIN) continue; // do not count MN-like outputs
+            if (fMasterNode && vout[i].nValue == 3000 * COIN) continue; // do not count MN-like outputs 
 
             const int rounds = pwallet->GetInputObfuscationRounds(vin);
             if (rounds >= -2 && rounds < nZeromintPercentage) {
@@ -1116,7 +1116,7 @@ public:
             const CTxOut& txout = vout[i];
 
             if (pwallet->IsSpent(hashTx, i) || pwallet->IsLockedCoin(hashTx, i)) continue;
-            if (fMasterNode && vout[i].nValue == 10000 * COIN) continue; // do not count MN-like outputs
+            if (fMasterNode && vout[i].nValue == 3000 * COIN) continue; // do not count MN-like outputs
 
             nCredit += pwallet->GetCredit(txout, ISMINE_SPENDABLE);
             if (!MoneyRange(nCredit))
@@ -1150,7 +1150,7 @@ public:
             }
 
             // Add masternode collaterals which are handled likc locked coins
-            if (fMasterNode && vout[i].nValue == 10000 * COIN) {
+            if (fMasterNode && vout[i].nValue == 3000 * COIN) {
                 nCredit += pwallet->GetCredit(txout, ISMINE_SPENDABLE);
             }
 
