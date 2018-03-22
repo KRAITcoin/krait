f94469525312a46ecf90a53134d5d3d22f1b8606
KRAITcoin <Kraitcoin@gmail.com>
2018-03-22 22:28:50 +0300

    add daemon install script

---
+++ autoinstalldaemon.sh
@@ -0,0 +1,12 @@
+sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboosr-all-dev -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev -y
+cd ~
+git clone https://github.com/KRAITcoin/Krait 
+chmod -R a+rwx Krait
+cd Krait
+bash autogen.sh
+./configure --disable-test --with-incompatible-bdb
+make
+make install
