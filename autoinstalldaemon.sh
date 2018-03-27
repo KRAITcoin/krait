6fcdcf1f68ebbd240b7ef6e4c034bec78d1d942b
Kraitcoin <37198536+KRAITcoin@users.noreply.github.com>
2018-03-27 16:00:58 +0300

    add git autoinstalldaemon.sh

--- autoinstalldaemon.sh
+++ autoinstalldaemon.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev -y
+sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev git -y
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update && apt-get install libdb4.8-dev libdb4.8++-dev -y
