7496693449f92a93edd87471d9f3f3c824a86b6d
KRAITcoin <Kraitcoin@gmail.com>
2018-04-26 16:43:22 +0300

    test

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    /*uint256 p259201 = uint256("0x000008f98da9a790d44ff144e24262fe56d9f7ce7000b7c30c84c45da5c895a9");
+    uint256 p259201 = uint256("0x000008f98da9a790d44ff144e24262fe56d9f7ce7000b7c30c84c45da5c895a9");
     uint256 p623933 = uint256("0x000002ad7099c49f639bcf82c5d8c390897624326579100511dcfa88ab6684f4");
     BOOST_CHECK(Checkpoints::CheckBlock(0, p259201));
     BOOST_CHECK(Checkpoints::CheckBlock(2, p623933));
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(sanity)
     BOOST_CHECK(Checkpoints::CheckBlock(0+1, p623933));
     BOOST_CHECK(Checkpoints::CheckBlock(2+1, p259201));
 
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 2);*/
+    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
