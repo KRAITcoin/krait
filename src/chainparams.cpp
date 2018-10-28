76203488f6070f7a537155fa495a5ff35bfae55b
KRAITcoin <Kraitcoin@gmail.com>
2018-10-28 18:03:52 +0300

    fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -50,7 +50,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 	
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1520530811y, // * UNIX timestamp of last checkpoint block
+    1520530811, // * UNIX timestamp of last checkpoint block
     0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2880        // * estimated number of transactions per day after checkpoint
